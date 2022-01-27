6c1ff8570f32fc3363d72692f1249d61a7d7084c
Piotr Lewandowski <piotrl@Piotrs-MacBook-Pro.local>
2022-01-27 10:58:46 -0500

    imported restuarants .json

---
+++ index.js
@@ -0,0 +1,11 @@
+// Import our restaurants ('./') pulls up all files in the folder, choose the right file
+const restuarants = require('./restaurants.json');
+
+console.log(restuarants[0].name)
+
+// connect to Firestore
+
+// create a collection called "restaurants"
+
+// add each restaurant
+
